a3a82fae009096cc69bbb5a2f19dafed814d99a5
asrani <asrani.27@gmail.com>
2025-06-18 21:07:51 +0800

    f

--- _karisa copy.sql
+++ _karisa copy.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50739 (5.7.39)
  File Encoding         : 65001
 
- Date: 10/06/2025 11:20:27
+ Date: 18/06/2025 20:52:59
 */
 
 SET NAMES utf8mb4;
@@ -97,6 +97,33 @@ INSERT INTO `kategori` (`id`, `nama`, `created_at`, `updated_at`) VALUES (3, 'Vi
 COMMIT;
 
 -- ----------------------------
+-- Table structure for konsultasi
+-- ----------------------------
+DROP TABLE IF EXISTS `konsultasi`;
+CREATE TABLE `konsultasi` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pasien_id` int(11) DEFAULT NULL,
+  `dokter_id` int(11) DEFAULT NULL,
+  `chat_pasien` text,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `chat_dokter` text,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of konsultasi
+-- ----------------------------
+BEGIN;
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (1, 2, 2, 'sdf', '2025-06-18 19:54:48', '2025-06-18 19:54:48', NULL);
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (2, 2, 2, 'sdfsdf', '2025-06-18 19:58:25', '2025-06-18 19:58:25', NULL);
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (6, 2, 2, NULL, '2025-06-18 20:47:15', '2025-06-18 20:47:15', 'dfgdsdf');
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (7, 2, 2, NULL, '2025-06-18 20:50:53', '2025-06-18 20:50:53', 'okdsfdsf');
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (8, 2, 2, NULL, '2025-06-18 20:50:55', '2025-06-18 20:50:55', 'dfgsd');
+INSERT INTO `konsultasi` (`id`, `pasien_id`, `dokter_id`, `chat_pasien`, `created_at`, `updated_at`, `chat_dokter`) VALUES (9, 2, 2, 'ok', '2025-06-18 20:52:10', '2025-06-18 20:52:10', NULL);
+COMMIT;
+
+-- ----------------------------
 -- Table structure for pasien
 -- ----------------------------
 DROP TABLE IF EXISTS `pasien`;
@@ -121,6 +148,31 @@ CREATE TABLE `pasien` (
 -- ----------------------------
 BEGIN;
 INSERT INTO `pasien` (`id`, `nama`, `umur`, `nik`, `jkel`, `tgl_lahir`, `alamat`, `telp`, `created_at`, `updated_at`, `user_id`, `foto`) VALUES (2, 'andi', NULL, NULL, NULL, NULL, 'jl pramuka', '08777899', '2025-06-09 00:44:27', '2025-06-09 00:44:27', 15, NULL);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for rekam_medis
+-- ----------------------------
+DROP TABLE IF EXISTS `rekam_medis`;
+CREATE TABLE `rekam_medis` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  `dokter_id` int(11) DEFAULT NULL,
+  `tanggal` varchar(255) DEFAULT NULL,
+  `nomor` varchar(255) DEFAULT NULL,
+  `keluhan` text,
+  `nik` varchar(255) DEFAULT NULL,
+  `obat` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of rekam_medis
+-- ----------------------------
+BEGIN;
+INSERT INTO `rekam_medis` (`id`, `nama`, `dokter_id`, `tanggal`, `nomor`, `keluhan`, `nik`, `obat`, `created_at`, `updated_at`) VALUES (1, 'klj', NULL, '2025-06-05', 'fg', 'lkjl', 'sdfkj', 'kj', '2025-06-18 20:25:59', '2025-06-18 20:26:21');
 COMMIT;
 
 -- ----------------------------
@@ -169,12 +221,12 @@ CREATE TABLE `users` (
 -- Records of users
 -- ----------------------------
 BEGIN;
-INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (1, 'superadmin', 'superadmin', '$2y$12$r0HAFQIZdiAabhk3HwCdVub716cax1jMnmwKnv76nJz8sJx0M3TB6', 'eWKD6FGB8sFTCiM0pb30a84lNvtUqkFJJDZOphR0mAEQyV4CQR1dJd5tZHVI', NULL, '2024-12-20 02:49:44', 'superadmin');
+INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (1, 'superadmin', 'superadmin', '$2y$12$r0HAFQIZdiAabhk3HwCdVub716cax1jMnmwKnv76nJz8sJx0M3TB6', 'pMZSkaWGFH3mvG2FC7Ic2pob4cRZQr6DWTGXBsaihYDrTDXqJ5UQXnaYnqWG', NULL, '2024-12-20 02:49:44', 'superadmin');
 INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (11, 'adi', 'adi2', '$2y$12$S8y2eXzZhf7OMrScCfdwT.9EZo6yv7EBZUkrk/faHh3DNzW/7zhPu', NULL, '2025-05-12 23:54:44', '2025-05-12 23:56:31', 'superadmin');
 INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (12, 'pu1', NULL, '$2y$10$zGhW7n6aBtBIgJ.moSRNtOnohY/MiTF6pcfL//qd4akj1T9U8oMwy', NULL, '2025-06-03 07:20:53', '2025-06-03 07:20:53', 'dokter');
-INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (13, 'dokter2', NULL, '$2y$10$dfHQaP3dKFALmnItqrVCEeRvzDNjWn3IKbkUrihAGZImGKP/cGb5C', '2arJJKrbA5Jk5SH1se8Y4Dbix12cLd1YwXmIOuwTfIuWnE3NZXiF3dePiXED', '2025-06-03 07:23:51', '2025-06-03 07:23:51', 'dokter');
+INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (13, 'dokter2', NULL, '$2y$10$vygcXV9TgNYpCnltnbXxqeuAQDhiR7n2ypsa3FvWuTY347PSIAo3m', 'RlNOJ0ptuUbZ1sIvdnrRJe2X3jSZWjzM3CW5M1zfWNIEg11a9FOScFhUaOUv', '2025-06-03 07:23:51', '2025-06-18 20:07:45', 'dokter');
 INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (14, 'pasien1', NULL, '$2y$10$gGgvmda5sOw.XteWdJKvoeQNJh1c.irtDx1Rol7t9.XLE3Q87aoqm', NULL, '2025-06-03 07:28:22', '2025-06-03 07:28:22', 'pasien');
-INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (15, 'andi', 'andi', '$2y$10$LopyX9Hl/T.iawe0samETeZCtneKeV8yOY8rs0JXtfat65nt6Nva.', NULL, '2025-06-09 00:44:27', '2025-06-09 00:44:27', 'pasien');
+INSERT INTO `users` (`id`, `username`, `name`, `password`, `remember_token`, `created_at`, `updated_at`, `roles`) VALUES (15, 'andi', 'andi', '$2y$10$yZdmxyf8pr2ytTowZZSIIOsh44zaDlDbN4oFZzk2iubBrBxkinMT6', 'FYaxAtmregMaGruLPk9B2913qjsUcXkj4b7W55vioEcMCzkl2h7Oy0itv5SG', '2025-06-09 00:44:27', '2025-06-18 19:30:03', 'pasien');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
