651096e3af8ac49aab49950f9a30175b0c94fd88
asrani <asrani.27@gmail.com>
2025-06-18 19:55:38 +0800

    f

--- app/Http/Controllers/KonsultasiController.php
+++ app/Http/Controllers/KonsultasiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Dokter;
 use App\Models\Jadwal;
+use App\Models\Konsultasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,7 +23,8 @@ class KonsultasiController extends Controller
     public function chat($id)
     {
         $dokter = Dokter::find($id);
-        return view('pasien.konsultasi.chat', compact('dokter', 'id'));
+        $chat = Konsultasi::where('pasien_id', Auth::user()->pasien->id)->where('dokter_id', $id)->get();
+        return view('pasien.konsultasi.chat', compact('dokter', 'id', 'chat'));
     }
 
     public function kirimChat(Request $req, $dokter_id)
@@ -31,8 +33,7 @@ class KonsultasiController extends Controller
         $param = $req->all();
         $param['pasien_id'] = Auth::user()->pasien->id;
         $param['dokter_id'] = $dokter_id;
-
-        Konsultasi::created($param);
+        Konsultasi::create($param);
         toastr()->success('Chat dikirim');
         return back();
     }
